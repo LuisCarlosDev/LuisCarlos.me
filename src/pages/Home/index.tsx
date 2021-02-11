d2ecec96e59085af58ac50b4f5f4b7749cac2f3e
Luis Carlos <luiscarlossilva.dev@gmail.com>
2021-02-11 11:15:31 -0300

    conclusao provisoria

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { motion } from 'framer-motion'
+
+import profileImg from '../../assets/logo.jpg'
+
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+// import { MdEmail } from 'react-icons/md';
+
+import { Container, Profile, ProfileContent, ListSites, GitHub, LinkeDin, Instagram, } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      
+      <Profile>
+        <ProfileContent>
+         <motion.img 
+            src={profileImg}
+            animate={{
+              scale: [1, 2, 2, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ["20%", "20%", "50%", "20%", "50%"],
+            }}
+         />
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 2.5 }}
+          >
+
+          <h1>Luis Carlos silva</h1>
+
+          </motion.div>
+        </ProfileContent>
+      </Profile>
+
+      <ListSites>
+          <GitHub href="https://github.com/LuisCarlosDev">
+            <FaGithub size={45} />
+            <h2>GitHub</h2>
+          </GitHub>
+
+          <LinkeDin href="https://www.linkedin.com/in/luis-carlos-silva-8856881b4/">
+            <FaLinkedin size={45} />
+            <h2>Linkedin</h2>
+          </LinkeDin>
+
+         {/*  <Email href="luiscarlossilva.dev@gmail.com">
+            <MdEmail size={45} />
+            <h2>E-mail</h2>
+          </Email> */}
+
+          <Instagram href="https://www.instagram.com/luis_carlos__ofc" >
+            <FaInstagram size={45} />
+            <h2>Instagram</h2>
+          </Instagram>
+        </ListSites>
     </Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,93 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Profile = styled.div`
+  margin-top: 20px;
+  img {
+    border: 5px solid #03b352;
+    border-radius: 50%;
+    max-width: 220px;
+  }
+
+  h1 {
+    font-size: 30px;
+    margin-top: 10px;
+  }
+`;
+
+export const ProfileContent = styled.div`
   
 `;
+
+export const ListSites = styled.div`
+  margin-top: 25px;
+  display: flex;
+
+  padding: 50px;
+`;
+
+export const GitHub = styled.a`
+  text-decoration: none;
+  justify-content: center;
+  text-align: center;
+  width: 150px;
+  color: #f0f0f5;
+  border-radius: 10px;
+  cursor: pointer;
+
+  background-image: linear-gradient(to bottom  right, #6e40c9, #03b352);
+
+  padding-top: 5px
+`;
+
+export const LinkeDin = styled.a`
+  text-decoration: none;
+  margin-left: 16px;
+  justify-content: center;
+  text-align: center;
+  width: 150px;
+  color: #f0f0f5;
+  border-radius: 10px;
+  cursor: pointer;
+
+  background-image: linear-gradient(to bottom  right, #004182, #f9fafb);
+
+  padding-top: 5px
+`;
+
+export const Email = styled.a`
+  text-decoration: none;
+  margin-left: 16px;
+  justify-content: center;
+  text-align: center;
+  width: 150px;
+  color: #f0f0f5;
+  border-radius: 10px;
+  cursor: pointer;
+
+  background-image: linear-gradient(to bottom  right, #4285f4, #bb001b, #ea4335, #fbbc05, #34a853);
+
+  padding-top: 5px
+`;
+
+export const Instagram = styled.a`
+  text-decoration: none;
+  margin-left: 16px;
+  justify-content: center;
+  text-align: center;
+  width: 150px;
+  color: #f0f0f5;
+  border-radius: 10px;
+  cursor: pointer;
+
+  background-image: linear-gradient(to bottom  right, #dd2a7b, #feda77);
+
+  padding-top: 5px
+`;
+
